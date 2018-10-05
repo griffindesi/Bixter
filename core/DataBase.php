8e7bf97a44e0ed37484b552331ae84175ef9e5e7
dr.monster <aboodma@gmail.com>
2018-10-05 10:23:07 +0300

    Add Update Statment

--- core/DataBase.php
+++ core/DataBase.php
@@ -7,7 +7,7 @@
  class DataBase {
 
   private static $_instance= null /* this will be the connection handler */;
-  private $_pdo/* this will be the connection object handler */
+  private $_pdo /* this will be the connection object handler */
   , $_query /* this will be the query handler */
   , $_error = false /* this will be the errors handler */
   , $_resault=0 /* this will be the the resault handler */
@@ -101,6 +101,45 @@
 
    }
      return $this;
+
+  }
+  
+  public function Update ( $table , $id , $fields = [])  {
+
+        $fieldString = '';
+
+        $valueString = '';
+
+        $values = [];
+
+
+        foreach ($fields as $field => $value) {
+          $fieldString .= '`' . $field . '`,';
+
+          $valueString .= '?,';
+
+          $values [] = $value;
+
+        }
+
+        $fieldString = trim($fieldString,',');
+
+        $valueString = rtrim($valueString,',');
+
+        $sql = "UPDATE {$table} SET {$fieldString}={$valueString} WHERE id={$id}";
+
+        // dnd($sql);
+
+        if (!$this->query($sql , $values)->error()) {
+          return true;
+        }
+         return False;
+
+
+  }
+
+  public function Delete () {
+
 
   }
 
