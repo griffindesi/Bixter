290cbde5e5b7b9694e85868c315d189e9238e639
griffindesign <34406204+griffindesign@users.noreply.github.com>
2018-10-10 21:35:43 +0300

    Add Session & Cookie Classes And Start With Login And Sign up Model And Controller And View

--- App/controllers/Home.php
+++ App/controllers/Home.php
@@ -13,44 +13,6 @@ class Home extends Controller
 
 public function indexAction()
 {
-  $DB= DataBase::getInstance();
-   $table="users";
-   $fields=[
-        'conditions' => ['phone=05375280964','fname=?'] ,
-         'bind' =>['05375280964'],
-         'order' =>"fname,mail",
-         'limit' =>5
-       ];
-   $colunms=$DB->find('users',[
-      'conditions' =>['phone = 020202025'] ,
-         'bind' =>['020202025'],
-         'order' =>"fname,mail",
-         'limit' =>5
-       ]);
-    dnd($colunms);
+
   $this->view->render('home/index');
-}
-public function  newAction() {
-
-  $DB= DataBase::getInstance();
-  $table="users ";
-  $fields=[
-        'id'=>'',
-        'fname'=>'Hussain Mardini',
-        'phone'=>'05375280964',
-        'mail'=>'aboodma@gmail.com'
-      ];
-  $newUser=$DB->insert($table,$fields);
-
-  dnd($newUser);
-}
-
-public function deleteAction()
-{
-
-  $DB= DataBase::getInstance();
-  $deleteUser=$DB->Delete('users',2);
-  dnd($deleteUser);
-}
-
 }
